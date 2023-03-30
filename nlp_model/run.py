73f16eb3e330430ada2675133f6c552325236601
Suraj25P <surajrprabhu99@gmail.com>
2023-03-30 14:15:57 +0530

    Final chages

--- nlp_model/run.py
+++ nlp_model/run.py
@@ -8,7 +8,7 @@ Master or Ph.D. in Mathematics, Physics, Computer Science or a similar field
 Three to six years of professional experience outside academia, preferably experience in Supply Chain
 Strong skills in Machine Learning, Statistics and Predictive Modelling
 Project management experience
-Programming skills, e.g. Python, R, Java, C++
+Programming skills, e.g. Python, Java,
 Comfortable reporting results to both technical and non-technical audiences
 Ability to work with people from various backgrounds/cultures
 Strong interpersonal skills with the desire to share knowledge and skills
@@ -25,6 +25,6 @@ def run_model(ner_model_path, job_description_text=job_description):
     for ent in doc.ents:
         skills_required[ent.label_] = []
     for ent in doc.ents:
-        #print(ent.text, '--->', ent.label_)
-        skills_required[ent.label_] += [ent.text]
+        print(ent.text, '--->', ent.label_)
+        skills_required[ent.label_] += [ent.text.lower()]
     return skills_required
